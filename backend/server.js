c9ef00999ce0288db6dce56a0ad5c3da6dbea584
Krishna <anonymous@google.com>
2025-10-30 19:10:27 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,5 @@ app.use('/api/auth',authRoutes)
 app.use('/api/resume',resumeRoutes)
 app.use('/api/otp',otpRoutes)
 
-app.listen(process.env.PORT||5000,()=>{
-    console.log("App is running")
-})
 
 export default app;
